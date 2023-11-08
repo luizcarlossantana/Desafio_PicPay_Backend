2203dccf5ba9dc91d785446d1af43ac680d8fc42
Luiz Carlos Santana <112254950+luizcarlossantana@users.noreply.github.com>
2023-11-08 17:54:25 -0300

    Criando api com spring initializr

---
+++ api/src/test/java/com/SuperBank/api/SuperBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SuperBank.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
