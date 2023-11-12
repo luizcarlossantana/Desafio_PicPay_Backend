2c5efadf41aea092f590c2e7ab03e60ac8f4bc83
Luiz Carlos Santana <112254950+luizcarlossantana@users.noreply.github.com>
2023-11-12 15:12:22 -0300

    Colocando a configuração do ModelMapper

--- api/src/main/java/com/SuperBank/api/SuperBankApplication.java
+++ api/src/main/java/com/SuperBank/api/SuperBankApplication.java
@@ -1,7 +1,9 @@
 package com.SuperBank.api;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SuperBankApplication {
@@ -10,4 +12,8 @@ public class SuperBankApplication {
 		SpringApplication.run(SuperBankApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 }
